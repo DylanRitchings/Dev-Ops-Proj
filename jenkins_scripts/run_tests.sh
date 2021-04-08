c93459fe4a9b7b94d31c044f2f688b741ef3e8d3
dylanritchings <dylanritchings1998@gmail.com>
2021-04-08 13:05:25 +0100

    DO-3 Edited jenkins files

--- jenkins_scripts/run_tests.sh
+++ jenkins_scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 build_ip=$(cat files/build_ip)
-ssh -i -t -t /home/jenkins/.ssh/vmkey.pem ubuntu@"${build_ip}" << EOF
+ssh -tt /home/jenkins/.ssh/vmkey.pem ubuntu@"${build_ip}" << EOF
     ls
     sudo rm -r Dev-Ops-Proj
     git clone https://github.com/DylanRitchings/Dev-Ops-Proj.git
