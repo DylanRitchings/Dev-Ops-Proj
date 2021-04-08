259fa3201939403e247c2010cdb10293b4c54f68
dylanritchings <dylanritchings1998@gmail.com>
2021-04-08 21:16:45 +0100

    DO-3 Fixed tests

--- jenkins_scripts/dockerhub.sh
+++ jenkins_scripts/dockerhub.sh
@@ -2,7 +2,8 @@ sudo chmod 666 /var/run/docker.sock
 sudo docker login --username="${DOCKER_USER}" --password="${DOCKER_PASSWORD}"
 rm -r Dev-Ops-Proj
 git clone https://github.com/DylanRitchings/Dev-Ops-Proj.git
-
+pwd
+ls
 cd Dev-Ops-Proj/flask-app/backend
 docker build -t dylanrit/backend
 sudo docker push dylanrit/backend
