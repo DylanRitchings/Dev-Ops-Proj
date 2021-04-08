d4e4edc7a7fd105bc1f3be1fc32ef785ca82fc2b
dylanritchings <dylanritchings1998@gmail.com>
2021-04-08 12:33:09 +0100

    DO-3 Refactored and moved files

--- jenkins_scripts/run_tests.sh
+++ jenkins_scripts/run_tests.sh
@@ -5,6 +5,7 @@ sudo ssh -i /home/jenkins/.ssh/vmkey.pem ubuntu@"$(cat ../files/build_ip)" << EO
     git clone https://github.com/DylanRitchings/Dev-Ops-Proj.git
     cd Dev-Ops-Proj
     git checkout jenkins
+    cd flask-app
     sudo docker-compose up -d
     sudo docker exec backend bash -c "pytest tests/ --cov application"
     sudo docker exec frontend bash -c "pytest tests/ --cov application"
