e7915ec8af485de33b410d2814f3ec687f228798
dylanritchings <dylanritchings1998@gmail.com>
2021-04-08 21:13:04 +0100

    DO-3 Fixed tests

--- jenkins_scripts/dockerhub.sh
+++ jenkins_scripts/dockerhub.sh
@@ -1,3 +1,4 @@
+sudo chmod 666 /var/run/docker.sock
 sudo docker login --username="${DOCKER_USER}" --password="${DOCKER_PASSWORD}"
 rm -r Dev-Ops-Proj
 git clone https://github.com/DylanRitchings/Dev-Ops-Proj.git
