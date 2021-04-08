19ef0ccb542baea0c5a394fce198be44e6833820
dylanritchings <dylanritchings1998@gmail.com>
2021-04-08 13:45:17 +0100

    DO-3 Edited jenkins files

--- jenkins_scripts/run_tests.sh
+++ jenkins_scripts/run_tests.sh
@@ -9,7 +9,11 @@ ssh -i ~/.ssh/vmkey.pem "$build_ip" << EOF
     sudo rm -r Dev-Ops-Proj
     git clone https://github.com/DylanRitchings/Dev-Ops-Proj.git
     cd Dev-Ops-Proj
+
     git checkout jenkins
+    DATABASE_URI="ubuntu@$(cat files/prod_rds_endpoint)"
+    TEST_DATABASE_URI="ubuntu@$(cat files/test_rds_endpoint)"
+    DATABASE_SECRET_KEY='jksdfl9weir90238908f09sdf8hfds'
     cd flask-app
     sudo docker-compose up -d
     sudo docker exec backend bash -c "pytest tests/ --cov application"
