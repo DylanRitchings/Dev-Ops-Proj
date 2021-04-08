5198d288fe636f468437e8629f872f605c1f12a3
dylanritchings <dylanritchings1998@gmail.com>
2021-04-08 23:36:54 +0100

    DO-3 Upload to dockerhub

--- jenkins_scripts/dockerhub.sh
+++ jenkins_scripts/dockerhub.sh
@@ -1,13 +1,14 @@
-#sudo chmod 666 /var/run/docker.sock
+sudo chmod 666 /var/run/docker.sock
 #sudo docker login --username="${DOCKER_USER}" --password="${DOCKER_PASSWORD}"
 rm -r Dev-Ops-Proj
+alias pip="pip3"
 git clone https://github.com/DylanRitchings/Dev-Ops-Proj.git
 
 cd Dev-Ops-Proj
 git checkout jenkins
 cd flask-app
 pwd
-docker-compose up -d
+sudo docker-compose up -d
 #docker run -d -p 5001:5001 --name backend backend:latest
 sudo docker build backend -t dylanrit/backend
 sudo docker push dylanrit/backend
