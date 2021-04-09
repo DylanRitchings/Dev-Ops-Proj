30de8bccf341a80250d73bbdd2e9c120114710aa
dylanritchings <dylanritchings1998@gmail.com>
2021-04-09 08:14:53 +0100

    DO-3 Configure kuberenetes

--- jenkins_scripts/create-cluster.sh
+++ jenkins_scripts/create-cluster.sh
@@ -3,11 +3,9 @@ eksctl create cluster \
 --region eu-west-2 \
 --nodegroup-name DemoNodes \
 --nodes 2 --nodes-min 2 --nodes-max 3 \
---vpc-public-subnets=subnet-0cd0f3a60029debc8,subnet-03823887213dad1b7 \
---vpc-private-subnets=subnet-0bf94982cde5266f9,subnet-0c5d1317cb8534adc \
 --node-type t3.medium \
 --with-oidc --ssh-access --ssh-public-key vmkey --managed
 
-
-#--vpc-public-subnets subnet-08767c8fd42f9207d \
-#--vpc-private-subnets subnet-0c5d1317cb8534adc \
+#
+#--vpc-public-subnets=subnet-0cd0f3a60029debc8,subnet-03823887213dad1b7 \
+#--vpc-private-subnets=subnet-0bf94982cde5266f9,subnet-0c5d1317cb8534adc \
